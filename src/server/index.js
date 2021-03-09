384359e17047fa154ed0db433cbfc5fd80c74c1f
Juan Cardona <rocandante@gmail.com>
2021-03-08 20:23:37 -0500

    add express js

---
+++ src/server/index.js
@@ -0,0 +1,11 @@
+const app = require('express')
+
+const port = 3000
+
+app.get('/' (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => { 
+  console.log(`Server on port ${port}`)
+})
