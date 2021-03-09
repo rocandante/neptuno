e117a2dd043c29dde0992a27910b638e403ae0c7
Juan Cardona <rocandante@gmail.com>
2021-03-08 21:51:31 -0500

    add error.log a winston

--- src/server/logger.js
+++ src/server/logger.js
@@ -3,5 +3,10 @@ const { createLogger, format, transports } = require('winston')
 // Setup logger
 const logger = createLogger({
   format: format.simple(),
-  transports: [ new transports.Console()]
+  transports: [ 
+    new transports.Console(),
+    new transports.File({ filename: 'error.log' })
+  ]
 })
+
+module.exports = logger
