5826348076fe64fd8c48d4ce43e6fe6797122393
Juan Cardona <rocandante@gmail.com>
2021-03-08 21:09:45 -0500

    corregido error menor

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,9 @@
-const app = require('express')
+const express = require('express')
 
+const app = express()
 const port = 3000
 
-app.get('/' (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
