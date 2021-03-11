48879e4365cc879a6976facacf38c93e59cfca0b
Juan Cardona <rocandante@gmail.com>
2021-03-11 05:01:11 -0500

    se agrega un Schema de Mongoose y se aplica validacion basica

--- src/api/task/model.js
+++ src/api/task/model.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose')
 
-const task = {
-  title: String,
-  description: String,
-  completed: Boolean,
-  url: String,
-  dueDate: Date
-}
+const { Schema } = mongoose
+
+const task = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 128
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 255,
+    default: ''
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  url: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  dueDate: {
+    type: Date,
+    default: null
+  },
+}, {
+  timestamps: true
+})
 
 module.exports = mongoose.model('task', task)
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -10,9 +10,15 @@ const logger = require('../server/logger')
  * @param {*} next 
  */
 function errorHandler (err, req, res, next) {
-  const { statusCode = 500, message, level = 'error' } = err
+  const { message, level = 'error' } = err
+  let { statusCode = 500 } = err
   const log = `${logger.header(req)} ${statusCode} ${message}`
 
+  // Errores de validacion
+  if ( err.message.startsWith('ValidationError')) {
+    statusCode = 422
+  }
+  
   // Escribe el log
   logger[level](log)
   
